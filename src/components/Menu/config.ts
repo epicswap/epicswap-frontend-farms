c1f38ade902b02882587b4b906f57074f1f94e8c
이진우 <binarywoo@epiclions.net>
2021-07-19 19:31:56 +0900

    initial commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,105 @@
+import { MenuEntry } from '@epicswap/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: 'https://exchange.tobaccoswap.finance/',
+      },
+      {
+        label: 'Liquidity',
+        href: 'https://exchange.tobaccoswap.finance/#/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: 'Nests',
+    icon: 'PoolIcon',
+    href: '/nests',
+  },
+  // {
+  //   label: 'Pools',
+  //   icon: 'PoolIcon',
+  //   href: '/pools',
+  // },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: '/lottery',
+  // },
+  // {
+  //   label: 'NFT',
+  //   icon: 'NftIcon',
+  //   href: '/nft',
+  // },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'PancakeSwap',
+        href: 'https://pancakeswap.info/token/0xcBcc53a56de062307f375211B54bCa622d81A0Bb',
+      },
+      {
+        label: 'CoinGecko',
+        href: 'https://www.coingecko.com/en/coins/goose-finance',
+      },
+      {
+        label: 'CoinMarketCap',
+        href: 'https://coinmarketcap.com/currencies/goose-finance/',
+      },
+      {
+        label: 'AstroTools',
+        href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
+      },
+    ],
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Github',
+        href: 'https://github.com/goosedefi/',
+      },
+      {
+        label: 'Docs',
+        href: 'https://goosedefi.gitbook.io/goose-finance/',
+      },
+      {
+        label: 'Blog',
+        href: 'https://goosefinance.medium.com/',
+      },
+    ],
+  },
+  {
+    label: 'Partnerships/IFO',
+    icon: 'GooseIcon',
+    href: 'https://docs.google.com/forms/d/e/1FAIpQLSe7ycrw8Dq4C5Vjc9WNlRtTxEhFDB1Ny6jlAByZ2Y6qBo7SKg/viewform?usp=sf_link',
+  },
+  {
+    label: 'Audit by Hacken',
+    icon: 'AuditIcon',
+    href: 'https://www.goosedefi.com/files/hackenAudit.pdf',
+  },
+  {
+    label: 'Audit by CertiK',
+    icon: 'AuditIcon',
+    href: 'https://certik.org/projects/goose-finance',
+  },
+]
+
+export default config
---
+++ src/state/farms/fetchFarms.ts
@@ -0,0 +1,141 @@
+import BigNumber from 'bignumber.js'
+import erc20 from 'config/abi/erc20.json'
+import masterchefABI from 'config/abi/masterchef.json'
+import multicall from 'utils/multicall'
+import { getMasterChefAddress } from 'utils/addressHelpers'
+import farmsConfig from 'config/constants/farms'
+import { QuoteToken } from '../../config/constants/types'
+
+const CHAIN_ID = process.env.REACT_APP_CHAIN_ID
+const ZERO = new BigNumber(0)
+
+const fetchFarms = async () => {
+  const data = await Promise.all(
+    farmsConfig.map(async (farmConfig) => {
+      const { pid, lpAddresses, tokenAddresses, isTokenOnly, quoteTokenAdresses } = farmConfig
+      const lpAdress = lpAddresses[CHAIN_ID]
+
+      const calls = [
+        // Balance of token in the LP contract
+        {
+          address: tokenAddresses[CHAIN_ID],
+          name: 'balanceOf',
+          params: [lpAdress],
+        },
+        // Balance of quote token on LP contract
+        {
+          address: quoteTokenAdresses[CHAIN_ID],
+          name: 'balanceOf',
+          params: [lpAdress],
+        },
+        // Balance of LP tokens in the master chef contract
+        {
+          address: isTokenOnly ? tokenAddresses[CHAIN_ID] : lpAdress,
+          name: 'balanceOf',
+          params: [getMasterChefAddress()],
+        },
+        // Total supply of LP tokens
+        {
+          address: lpAdress,
+          name: 'totalSupply',
+        },
+        // Token decimals
+        {
+          address: tokenAddresses[CHAIN_ID],
+          name: 'decimals',
+        },
+        // Quote token decimals
+        {
+          address: farmConfig.quoteTokenAdresses[CHAIN_ID],
+          name: 'decimals',
+        },
+      ]
+
+      const [tokenBalanceLP, quoteTokenBlanceLP, lpTokenBalanceMC, lpTotalSupply, tokenDecimals, quoteTokenDecimals] =
+        await multicall(erc20, calls)
+
+      let tokenAmount;
+      let lpTotalInQuoteToken;
+      let tokenPriceVsQuote;
+      if(farmConfig.isTokenOnly){
+        tokenAmount = new BigNumber(lpTokenBalanceMC).div(new BigNumber(10).pow(tokenDecimals));
+        if(farmConfig.tokenSymbol === QuoteToken.BUSD && farmConfig.quoteTokenSymbol === QuoteToken.BUSD){
+          tokenPriceVsQuote = new BigNumber(1);
+        }else{
+          tokenPriceVsQuote = new BigNumber(quoteTokenBlanceLP).div(new BigNumber(tokenBalanceLP));
+        }
+        lpTotalInQuoteToken = tokenAmount.times(tokenPriceVsQuote);
+      }else{
+        // Ratio in % a LP tokens that are in staking, vs the total number in circulation
+        const lpTokenRatio = new BigNumber(lpTokenBalanceMC).div(new BigNumber(lpTotalSupply))
+
+        // Total value in staking in quote token value
+        lpTotalInQuoteToken = new BigNumber(quoteTokenBlanceLP)
+          .div(new BigNumber(10).pow(18))
+          .times(new BigNumber(2))
+          .times(lpTokenRatio)
+
+        // Amount of token in the LP that are considered staking (i.e amount of token * lp ratio)
+        tokenAmount = new BigNumber(tokenBalanceLP).div(new BigNumber(10).pow(tokenDecimals)).times(lpTokenRatio)
+        const quoteTokenAmount = new BigNumber(quoteTokenBlanceLP)
+          .div(new BigNumber(10).pow(quoteTokenDecimals))
+          .times(lpTokenRatio)
+
+        if(tokenAmount.comparedTo(0) > 0){
+          tokenPriceVsQuote = quoteTokenAmount.div(tokenAmount);
+        }else{
+          tokenPriceVsQuote = new BigNumber(quoteTokenBlanceLP).div(new BigNumber(tokenBalanceLP));
+        }
+      }
+
+      // Only make masterchef calls if farm has pid
+      const [info, totalAllocPoint, tbcPerBlock] =
+        pid || pid === 0
+          ? 
+          await multicall(masterchefABI, [
+              {
+                address: getMasterChefAddress(),
+                name: 'poolInfo',
+                params: [pid],
+              },
+              {
+                address: getMasterChefAddress(),
+                name: 'totalAllocPoint',
+              },
+              {
+                address: getMasterChefAddress(),
+                name: 'tbcPerBlock',
+              },
+            ])
+          : [null, null]
+
+      const allocPoint = info ? new BigNumber(info.allocPoint?._hex) : ZERO
+      const poolWeight = totalAllocPoint ? allocPoint.div(new BigNumber(totalAllocPoint)) : ZERO
+
+      // console.log('결과 tbcPerBlock', tbcPerBlock)
+      // console.log('결과 allocPoint', allocPoint)
+      // console.log('결과 tokenAmount', tokenAmount.toJSON())
+      // console.log('결과 lpTotalInQuoteToken', lpTotalInQuoteToken.toJSON())
+      // console.log('결과 tokenPriceVsQuote', tokenPriceVsQuote.toJSON())
+      // console.log('결과 poolWeight', poolWeight.toNumber())
+      // console.log('결과 info', info)
+      // console.log('결과 depositFeeBP', info ? info.depositFeeBP : 0)
+      // console.log('결과 multiplier', `${allocPoint.div(100).toString()}X`)
+      return {
+        ...farmConfig,
+        tokenAmount: tokenAmount.toJSON(),
+        // quoteTokenAmount: quoteTokenAmount.toJSON(),
+        lpTotalInQuoteToken: lpTotalInQuoteToken.toJSON(),
+        tokenPriceVsQuote: tokenPriceVsQuote.toJSON(),
+        poolWeight: poolWeight.toNumber(),
+        multiplier: `${allocPoint.div(100).toString()}X`,
+        depositFeeBP: info ? info.depositFeeBP : 0,
+        tbcPerBlock: new BigNumber(tbcPerBlock).toNumber(),
+      }
+    }),
+  )
+  console.log('끝', data)
+  return data
+}
+
+export default fetchFarms
