91982585833c48bdc1cc39ef7db252f5346fc112
이진우 <binarywoo@epiclions.net>
2021-07-21 16:41:22 +0900

    update twitter

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'SwapTobbaco',
+            screenName: 'epicswap1',
           }}
           options={{
             height: '300',
