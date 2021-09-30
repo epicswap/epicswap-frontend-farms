16efb134625bf3f63a9e66d4e8f064647315982d
brad kim <brad.kim@epiclions.net>
2021-09-30 18:26:51 +0900

    fix

--- src/utils/referrals.ts
+++ src/utils/referrals.ts
@@ -3,25 +3,20 @@ import { AbiItem } from 'web3-utils'
 import referralABI from 'config/abi/referral.json'
 import { getReferralAddress } from 'utils/addressHelpers'
 import { getWeb3 } from 'utils/web3'
-import CryptoJS from 'crypto-js'
-import ENC from 'crypto-js/enc-utf8'
+import { Base64 } from 'js-base64';
 
-// const CryptoJS = require('crypto-js')
+
 const web3 = getWeb3()
 const referralContract = new web3.eth.Contract(referralABI as unknown as AbiItem, getReferralAddress())
-const secretKey = CryptoJS.enc.Utf8.parse('\x65\x70\x69\x63\x73\x77\x61\x70')
 const hexPrefix = '0x'
 const zeroAddress = `${hexPrefix}0000000000000000000000000000000000000000`
 const epicRefCodeCookieKey = 'referral_code'
+// https://www.epicswap.io?ref=4UUx61jBMumJ7EZbiLly91hX9fM5HzuuGDejhQFl2I+EE7s/Mtn1kg
 
 export const generateReferralLink = (account) => {
   const simplifieldAccount = account.replace(hexPrefix, '')
-  const code = CryptoJS.Rabbit.encrypt(simplifieldAccount, secretKey, {
-    mode: CryptoJS.mode.ECB,
-    padding: CryptoJS.pad.NoPadding,
-  })
-    .toString()
-    .replace('==', '')
+  const code = Base64.encodeURI(simplifieldAccount).replace('==', '')
+
   const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://www.epicswap.io' : 'http://localhost:3000'
   return `${BASE_URL}?ref=${code}`
 }
@@ -53,12 +48,8 @@ export async function setReferralCode(referralCode, account) {
 
   // using searchParams.get('ref') functions removes some characters from the url
   const cleanRefferalCode = `${referralCode.replace('?ref=', '')}==`
-  const decryptedReferreAddress = CryptoJS.Rabbit.decrypt(cleanRefferalCode, secretKey, {
-    mode: CryptoJS.mode.ECB,
-    padding: CryptoJS.pad.NoPadding,
-  })
-
-  const newReferralAddress = decryptedReferreAddress.toString(ENC);
+  
+  const newReferralAddress = Base64.decode(cleanRefferalCode)
   const referrereAddress = `${hexPrefix}${newReferralAddress}`
 
   if (account === referrereAddress) {
@@ -78,12 +69,7 @@ export function getReferralCode() {
     return zeroAddress
   }
 
-  const decryptedReferrereAddress = CryptoJS.Rabbit.decrypt(cookieValue, secretKey, {
-    mode: CryptoJS.mode.ECB,
-    padding: CryptoJS.pad.NoPadding,
-  })
-  console.log("ENC",decryptedReferrereAddress)
-  const newReferralAddress = decryptedReferrereAddress.toString(ENC);
+  const newReferralAddress = Base64.decode(cookieValue)
 
   const referrereAddress = `${hexPrefix}${newReferralAddress}`
   document.cookie = `${epicRefCodeCookieKey}=; expires=Thu, 01 Jan 1970 00:00:00 UTC;`
